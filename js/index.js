cd2fbe434fc6fd5d501ff092ec570f23da8c5bf2
Lundkurt <77051800+Lundkurt@users.noreply.github.com>
2023-03-12 22:41:31 +0100

    adding placeholder Hero image

--- js/index.js
+++ js/index.js
@@ -7,18 +7,18 @@ createHeader();
 const featuredContainer = document.querySelector(".featured-container");
 const heroImg = document.querySelector(".hero-img");
 
-// async function fetchHero() {
-//   try {
-//     featuredContainer.innerHTML = "";
-//     const response = await fetch(baseUrl + "home");
-//     const hero = await response.json();
-//     heroImg.style.backgroundImage = `url("${hero.hero_banner.url}")`;
-//   } catch (error) {
-//     console.warn(error);
-//   }
-// }
+function fetchHero() {
+  try {
+    featuredContainer.innerHTML = "";
+    // const response = await fetch(baseUrl + "home");
+    // const hero = await response.json();
+    heroImg.style.backgroundImage = `url("../images/carousel-3.jpg")`;
+  } catch (error) {
+    console.warn(error);
+  }
+}
 
-// fetchHero();
+fetchHero();
 
 async function fetchFeatured() {
   try {
